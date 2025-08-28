5171b94280c1e0e5274bcc6f5082c7ce6c2d2909
Pennacchi <rodrigopennacchi@gmail.com>
2025-08-27 21:42:01 -0300

    Airflow, extraction, API Manager, operations

--- supermarket_extractions/src/market_001/update_etl_table.py
+++
@@ -1,9 +0,0 @@
-#! supermarket_extractions/venv/bin/python
-current_page = 1
-total_pages = 3
-
-print(f'Current page: {current_page} of {total_pages}.')
-while current_page <= total_pages:
-  print(f'Current page: {current_page} of {total_pages}.')
-  current_page += 1
-  total_pages = 0
